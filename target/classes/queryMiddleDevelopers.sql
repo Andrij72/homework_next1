855bc53f8ffedf76edfe2cbe0af83a1540450ee1
Andrij Kulynych <akulmmm@gmail.com>
2019-04-17 17:35:01 +0300

    Some correct gitignor JPIapplication homewrk 15

--- target/classes/queryMiddleDevelopers.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM developers
-INNER JOIN skill_developer ON developers.id = skill_developer.id_developer
-INNER JOIN skills ON skill_developer.id_skill = skills.id
-WHERE skills.level = ?;
--- target/classes/testqueryCreateTabl.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE developers(ID INT NOT NULL,
-                               NAME VARCHAR (100) NOT NULL,
-                                                  SEX VARCHAR (20) NOT NULL,
-                                                                   AGE INT(3) NOT NULL,
-                                                                              SPECIALTY VARCHAR(100) NOT NULL,
-                                                                                                     SALARY INT NOT NULL,
-                                                                                                                PRIMARY KEY (ID));
